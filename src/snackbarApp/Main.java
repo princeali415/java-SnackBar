9ace50c2f500b9ab36605e019fe048c750a78e4e
Hussain Ali <ali.415code@gmail.com>
2021-02-08 12:49:29 -0800

    first commit

--- src/snackbarApp/Main.java
+++ src/snackbarApp/Main.java
@@ -2,8 +2,13 @@ package snackbarApp;
 
 public class Main 
 {
+    private static void workingWithData()
+    {
+        System.out.println("Java snackbar program running...");
+    }
+
     public static void main(String[] args) 
     {
-        
+        workingWithData();
     }
 }
